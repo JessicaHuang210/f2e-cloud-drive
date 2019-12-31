3b445805ae9433d4c8515a8e264ae35e2f388f97
黃婉柔 <>
2019-12-31 17:40:20 +0800

    Updates

---
+++ precache-manifest.4a676a8321d910a29f2c732a1df2eaf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844fc6456d72c0e5640ab4f1419603f0",
+    "url": "/f2e-cloud-drive/index.html"
+  },
+  {
+    "revision": "b204a95373e93da51b31",
+    "url": "/f2e-cloud-drive/static/js/2.694d6cad.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/f2e-cloud-drive/static/js/2.694d6cad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e70f16b81485a36893f",
+    "url": "/f2e-cloud-drive/static/js/main.bfa969a0.chunk.js"
+  },
+  {
+    "revision": "679c93c47f8b5e93aaf8",
+    "url": "/f2e-cloud-drive/static/js/runtime-main.14d61b4c.js"
+  },
+  {
+    "revision": "18120ba2402db651e49068b70122ff72",
+    "url": "/f2e-cloud-drive/static/media/Cloud-monster.18120ba2.svg"
+  },
+  {
+    "revision": "bc3a66211c91483450160add996b077f",
+    "url": "/f2e-cloud-drive/static/media/logo.bc3a6621.svg"
+  }
+]);
--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import GlobalLayout from "container/GlobalLayout.js";
-export default class App extends Component {
-  render() {
-    return <GlobalLayout />;
-  }
-}
