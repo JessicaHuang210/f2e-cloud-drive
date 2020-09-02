1a963177bb37671f72eeb946293dc00989b518d9
Jessica <jessica.huang@silkrode.com.tw>
2020-09-02 09:30:16 +0800

    update

--- src/components/GlobalLayout/globalStyle.js
+++ src/components/GlobalLayout/globalStyle.js
@@ -1,82 +1,82 @@
 import styled, { createGlobalStyle } from "styled-components";
 export const ResetStyle = createGlobalStyle`
     html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-    box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+        box-sizing: border-box;
     }
     address, caption, cite, code, dfn, em, strong, th, var, b {
-    font-weight: normal;
-    font-style: normal;
+        font-weight: normal;
+        font-style: normal;
     }
     abbr, acronym {
-    border: 0;
+        border: 0;
     }
     article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
-    display: block;
+        display: block;
     }
     *,
     *::after,
     *::before {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
+        margin: 0;
+        padding: 0;
+        box-sizing: inherit;
     }
     html {
-    text-size-adjust: 100%;
-    box-sizing: border-box;
+        text-size-adjust: 100%;
+        box-sizing: border-box;
     }
     body {
         line-height: 1;
     }
     ol, ul {
-    list-style: none;
+        list-style: none;
     }
     blockquote, q {
-    quotes: none;
+        quotes: none;
     }
     blockquote {
-    &:before,   &:after {
-        content: '';
-        content: none;
-    }
+        &:before,   &:after {
+            content: '';
+            content: none;
+        }
     }
     q {
-    &:before,   &:after {
-        content: '';
-        content: none;
-    }
+        &:before,   &:after {
+            content: '';
+            content: none;
+        }
     }
     table {
-    border-collapse: collapse;
-    border-spacing: 0;
+        border-collapse: collapse;
+        border-spacing: 0;
     }
     caption, th {
-    text-align: left;
+        text-align: left;
     }
     textarea {
-    resize: none;
+        resize: none;
     }
     a {
-    text-decoration: none;
-    cursor: pointer;
+        text-decoration: none;
+        cursor: pointer;
     }
     button {
-    padding: 0;
-    border: none;
-    background: none;
+        padding: 0;
+        border: none;
+        background: none;
     }
 `;
 
 export const GlobalStyle = createGlobalStyle`
     html {
-    box-sizing: border-box;
-    font-size: 62.5%; 
-    font-family: 'HelveticaNeue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+        box-sizing: border-box;
+        font-size: 62.5%; 
+        font-family: 'HelveticaNeue', Helvetica, Arial, 'Lucida Grande', sans-serif;
     }
     body {
         background-color: #25272C;
